6c05a06ecfdce44abc572951eca951cea383609e
Lenin <lenindpulgar@gmail.com>
2016-02-19 12:27:46 -0430

    19-02-2016
    Latest update

--- footer.php
+++ footer.php
@@ -1,38 +1,29 @@
 <div class="row">
 			<footer>
 			   <div class="row">
-				<div class="medium-4 column">
-					<h3>Capt Bubba Carter</h3>
-					<p style="font-size:1.1em;">Sport Fishing in Costa Rica</p>
-					<hr class="style-one">
-					<div class="redesf">
-						<span class="ss-icon ss-social-regular">Facebook</span>
-						
-						<span class="ss-icon ss-social-regular">Twitter</span>
-						
-						<span class="ss-icon ss-social-regular">Mail</span>
-						
-						<span class="ss-icon">cell</span>
-						
-						<span class="ss-icon">phone</span>
-					</div>
+
+
+				<div id="footer_1" class="medium-4 column">
+					<?php
+					if(is_active_sidebar('footer_1')){
+					dynamic_sidebar('footer_1');
+					}
+					?>
 				</div>
-				<div class="medium-4 column">
-					<p><strong>Sitemap</strong></p>
-					<ul>
-						<li><a href="">Sport fishing</a></li>
-						<li><a href="">Sea mount fishing</a></li>
-						<li><a href="">Accommodations</a></li>
-						<li><a href="">Contact</a></li>
-						<li><a href="">Gallery</a></li>
-						<li><a href="">Fishing Report</a></li>
-					</ul>
+
+				<div id="footer_2" class="medium-4 column">
+					<?php
+					if(is_active_sidebar('footer_2')){
+					dynamic_sidebar('footer_2');
+					}
+					?>
 				</div>
-				<adress class="medium-4 column">
-					<p><strong>Call us now!</strong></p>
-					<p><span class="ss-phone" style="color:rgb(74,152,218);"></span> 506 2637 8520</p>
-					<p><span class="ss-cell" style="color:rgb(74,152,218);"></span> 011 506 8650 2203</p>
-					<p><span class="ss-mail" style="color:rgb(74,152,218);"></span> captbubbacarter@yahoo.com</p>
+				<adress id="footer_3" class="medium-4 column">
+					<?php
+					if(is_active_sidebar('footer_3')){
+					dynamic_sidebar('footer_3');
+					}
+					?>
 				</adress>
 				</div>
 			</footer><!--Fin del footer-->
--- functions.php
+++ functions.php
@@ -44,9 +44,140 @@ function bubba_theme_setup() {
 	add_theme_support('menus');
 	
 	register_nav_menu('primary', 'Primary Header Navigation');
-	register_nav_menu('secondary', 'Footer Navigation');
+	register_nav_menu('secondary', 'Footer Navigation', array());
 	
 }
 
 
 add_action('init', 'bubba_theme_setup');
+
+//Function widget areas
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function footer_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Footer 1',
+		'id'            => 'footer_1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="">',
+		'after_title'   => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name'          => 'Footer_2',
+		'id'            => 'footer_2',
+		'class'			=> 'none',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<p class=""><strong>',
+		'after_title'   => '</strong></p>',
+	) );
+
+	register_sidebar( array(
+		'name'          => 'Footer_3',
+		'id'            => 'footer_3',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<p class=""><strong>',
+		'after_title'   => '</strong></p>',
+	) );
+
+}
+add_action( 'widgets_init', 'footer_widgets_init' );
+
+
+
+/**
+ * WTI Custom Navigation Menu widget class
+ *
+ */
+
+class Wti_Custom_Nav_Menu_Widget extends WP_Widget {
+
+    function __construct() {
+        $widget_ops = array( 'description' => __('Use this widget to add one of your custom menus as a widget.') );
+        parent::__construct( 'custom_nav_menu', __('WTI Custom Menu'), $widget_ops );
+    }
+
+    function widget($args, $instance) {
+        // Get menu
+        $nav_menu = ! empty( $instance['nav_menu'] ) ? wp_get_nav_menu_object( $instance['nav_menu'] ) : false;
+
+        if ( !$nav_menu )
+            return;
+
+        $instance['title'] = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+
+        echo $args['before_widget'];
+
+        if ( !empty($instance['title']) )
+            echo $args['before_title'] . $instance['title'] . $args['after_title'];
+
+        wp_nav_menu(
+                array(
+                    'fallback_cb' => '',
+                    'container' => '',
+                    'menu_class' => $instance['menu_class'],
+                    'menu' => $nav_menu
+                )
+            );
+
+        echo $args['after_widget'];
+    }
+
+    function update( $new_instance, $old_instance ) {
+        $instance['title'] = strip_tags ( stripslashes ( $new_instance['title'] ) );
+        $instance['menu_class'] = strip_tags ( stripslashes ( trim ( $new_instance['menu_class'] ) ) );
+        $instance['nav_menu'] = (int) $new_instance['nav_menu'];
+
+        return $instance;
+    }
+
+    function form( $instance ) {
+        $title = isset( $instance['title'] ) ? $instance['title'] : '';
+        $menu_class = isset( $instance['menu_class'] ) ? $instance['menu_class'] : '';
+        $nav_menu = isset( $instance['nav_menu'] ) ? $instance['nav_menu'] : '';
+
+        // Get menus
+        $menus = get_terms( 'nav_menu', array( 'hide_empty' => false ) );
+
+        // If no menus exists, direct the user to go and create some.
+        if ( !$menus ) {
+            echo '<p>'. sprintf( __('No menus have been created yet. <a href="%s">Create some</a>.'), admin_url('nav-menus.php') ) .'</p>';
+            return;
+        }
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:') ?></label>
+            <input type="text" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" />
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('menu_class'); ?>"><?php _e('Menu Class:') ?></label>
+            <input type="text" class="widefat" id="<?php echo $this->get_field_id('menu_class'); ?>" name="<?php echo $this->get_field_name('menu_class'); ?>" value="<?php echo $menu_class; ?>" />
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('nav_menu'); ?>"><?php _e('Select Menu:'); ?></label>
+            <select id="<?php echo $this->get_field_id('nav_menu'); ?>" name="<?php echo $this->get_field_name('nav_menu'); ?>">
+        <?php
+            foreach ( $menus as $menu ) {
+                echo '<option value="' . $menu->term_id . '"'
+                    . selected( $nav_menu, $menu->term_id, false )
+                    . '>'. $menu->name . '</option>';
+            }
+        ?>
+            </select>
+        </p>
+        <?php
+    }
+}
+
+function wti_custom_nav_menu_widget() {
+    register_widget('Wti_Custom_Nav_Menu_Widget');
+}
+
+add_action ( 'widgets_init', 'wti_custom_nav_menu_widget', 1 );
